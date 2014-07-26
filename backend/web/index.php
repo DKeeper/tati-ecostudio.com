a5590b3f823a69698211329c66af6a1522afb619
Dmitry Kapenkin <dkapenkin@rambler.ru>
2014-07-26 22:31:58 +0400

    Fix .gitignore

---
+++ backend/web/index.php
@@ -0,0 +1,41 @@
+<?php
+if(getenv('WWW_ENV') == 'DEV'){
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+} else {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
+}
+
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/aliases.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/main.php'),
+    require(__DIR__ . '/../../common/config/main-local.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-local.php')
+);
+
+$application = new yii\web\Application($config);
+
+// Add rule for pages
+Yii::$app->on(\yii\base\Application::EVENT_BEFORE_REQUEST,function($event){
+    $names = [];
+    $rows = (new \yii\db\Query())
+        ->select('name')
+        ->from(common\models\Page::tableName())
+        ->where(['status'=>1])
+        ->orderBy('output_order')
+        ->all();
+    foreach($rows as $row){
+        $names[] = $row['name'];
+    }
+    $names = implode('|',$names);
+    $rule = ['<name('.$names.')>' => 'page/viewbyname'];
+    Yii::$app->urlManager->addRules($rule,false);
+    Yii::info(yii\helpers\VarDumper::dumpAsString(Yii::$app->urlManager->rules));
+});
+
+$application->run();
